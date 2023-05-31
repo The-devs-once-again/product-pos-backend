baa56dd02b251f77bd5e5bcc22b593f9b4d3991a
Phancak3 <phanqt.cs@gmail.com>
2023-06-01 00:48:49 +0800

    Product Models

--- product_app/models.py
+++ product_app/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+
+class Products(models.Model):
+    product_name = models.CharField(max_length=225)
+    product_price = models.FloatField()
