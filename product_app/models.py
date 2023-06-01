843ca2ee0bfab03e21a78eef906299908f076db7
Lemuel Guevara <lemueljohn.guevara@gmail.com>
2023-06-01 13:01:00 +0800

    Added category in product model and renamed "Products" to "Product"

--- product_app/models.py
+++ product_app/models.py
@@ -20,7 +20,8 @@ class Variation(models.Model):
     add_on = models.OneToOneField(AddOn, on_delete=models.CASCADE)
 
 
-class Products(models.Model):
+class Product(models.Model):
     product_name = models.CharField(max_length=225)
     product_price = models.OneToOneField(Price, on_delete=models.CASCADE)
+    category = models.CharField(max_length=255, default=" ")
     variation = models.OneToOneField(Variation, on_delete=models.CASCADE)
