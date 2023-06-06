1fa8b9a03042fad29c71602b478b9c053a374e16
Lemuel Guevara <lemueljohn.guevara@gmail.com>
2023-06-06 11:35:10 +0800

    PPS-44 Fixed type of 'update_product' endpoint

--- product_app/urls.py
+++ product_app/urls.py
@@ -4,7 +4,7 @@ from .views import ProductListView, ProductDetailView, CreatProductView, UpdateP
 
 urlpatterns = [
     path('create_product/', CreatProductView.as_view()),
-    path('upate_product/<int:pk>', UpdateProductView.as_view()),
+    path('update_product/<int:pk>', UpdateProductView.as_view()),
     path('products/', ProductListView.as_view()),
     path('products/<str:product_name>', ProductDetailView.as_view())
 ]
