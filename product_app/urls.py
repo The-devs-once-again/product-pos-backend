6bb379140c8f9a37ac142ad74a3401a8dc3207dd
Lemuel Guevara <lemueljohn.guevara@gmail.com>
2023-06-07 01:54:26 +0800

    Updated the urls of the product app

--- product_app/urls.py
+++ product_app/urls.py
@@ -5,6 +5,6 @@ from .views import ProductListView, ProductDetailView, CreatProductView, UpdateP
 urlpatterns = [
     path('create_product/', CreatProductView.as_view()),
     path('update_product/<int:pk>', UpdateProductView.as_view()),
-    path('products/', ProductListView.as_view()),
-    path('products/<str:product_name>', ProductDetailView.as_view())
+    path('product_list/', ProductListView.as_view()),
+    path('get_product/<str:product_name>', ProductDetailView.as_view())
 ]
