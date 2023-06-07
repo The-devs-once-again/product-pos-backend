7a92912da6fc49972b1feff5108bf5f78b53c5c3
Phancak3 <phanqt.cs@gmail.com>
2023-06-07 11:22:13 +0800

    PPS-56 #done #comment Updated!

--- product_app/models.py
+++ product_app/models.py
@@ -26,25 +26,3 @@ class Product(models.Model):
     product_type = models.CharField(max_length=255)
     category = models.CharField(max_length=255)
     variation = models.OneToOneField(Variation, on_delete=models.CASCADE, null=True, blank=True)
-
-
-class Order(models.Model):
-    order_date = models.DateTimeField(auto_now_add=True)
-    order_id = models.CharField(max_length=225)
-    order_status = models.CharField(max_length=225)
-    order_total = models.FloatField(Price)
-    order_details = models.ManyToManyField(Product)
-
-
-class OrderHistory(models.Model):
-    order_details = models.JSONField(Order)
-    date_created = models.DateTimeField(auto_now_add=True)
-    date_completed = models.DateTimeField(auto_now_add=True)
-
-
-class Billing(models.Model):
-    order_details = models.JSONField(Order, on_delete=models.CASCADE)
-    date_created = models.DateTimeField(auto_now_add=True)
-    date_completed = models.DateTimeField(auto_now_add=True)
-    payment_method = models.CharField(max_length=225)
-    amount = models.DecimalField(max_digits=10, decimal_places=2)
--- store_app/models.py
+++ store_app/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from product_app.models import Product
 
 # Create your models here.
+class Order(models.Model):
+    order_date = models.DateTimeField(auto_now_add=True)
+    order_id = models.CharField(max_length=225)
+    order_quantity = models.IntegerField()
+    order_status = models.CharField(max_length=225)
+    order_total = models.FloatField()
+    orders = models.ManyToManyField(Product)
+
+
+class OrderHistory(models.Model):
+    orders = models.ForeignKey(Order, on_delete=models.CASCADE)
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_completed = models.DateTimeField(auto_now_add=True)
+
+
+class Billing(models.Model):
+    orders = models.ForeignKey(Order, on_delete=models.CASCADE)
+    ref_id = models.CharField(max_length=225)
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_completed = models.DateTimeField(auto_now_add=True)
+    payment_method = models.CharField(max_length=225)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
