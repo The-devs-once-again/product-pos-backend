fbe6c0ba22e3a37279ac3c740989d2ece51fb74b
Lemuel Guevara <lemueljohn.guevara@gmail.com>
2023-06-12 23:56:44 +0800

    PPS-58 Added documentation

--- shared/factories/generic_views.py
+++ shared/factories/generic_views.py
@@ -1,10 +1,12 @@
+"""This file is responsible for making the generic views"""
+
 import json
 from abc import ABC, abstractmethod
 
 from django.db import models
 from django.http import JsonResponse
 from django.shortcuts import get_object_or_404
-from .view_config import GenericViewConfigLoader
+from shared.configs.view_config import GenericViewConfigLoader
 
 
 class _ABCGenericView(ABC, GenericViewConfigLoader):
@@ -12,6 +14,8 @@ class _ABCGenericView(ABC, GenericViewConfigLoader):
 
 
 class UpdateViewInterface(ABC):
+    """Absract interface that defines the methods of an UpdateVIew"""
+
     def update(self, request, pk=None, look_query=None, partial=False):
         """Gets the object to be updated"""
 
@@ -29,30 +33,40 @@ class UpdateViewInterface(ABC):
 
 
 class CreateViewInterface(ABC):
+    """Abstract interface that defines the methods of a CreateView"""
+
     @abstractmethod
     def post(self, request):
         """Creates an object"""
 
 
 class ListViewInterface(ABC):
+    """Abstract interface that defines the methods of a ListView"""
+
     @abstractmethod
     def get(self, request):
         """Lists all the objects"""
 
 
 class DetailViewInterface(ABC):
+    """Abstract interface that defines the methods of a DetailView"""
+
     @abstractmethod
     def get(self, request, query):
         """Gets a single object"""
 
 
 class DeleteAllViewInterface(ABC):
+    """Abstract interface that defines the methods of a DeleteAllView"""
+
     @abstractmethod
     def delete(self, request):
         """Deletes all objects"""
 
 
 class UpdateView(_ABCGenericView, UpdateViewInterface):
+    """Generic view that does the update methods"""
+
     def _get_model_class(self, pk=None, lookup_query=None) -> models.Model:
         if lookup_query is not None:
             model_class = get_object_or_404(self.view_config.model, **lookup_query)
@@ -87,6 +101,8 @@ class UpdateView(_ABCGenericView, UpdateViewInterface):
 
 
 class CreateView(_ABCGenericView, CreateViewInterface):
+    """Generic view that does creation of objects in the database"""
+
     def post(self, request):
         data = json.loads(request.body.decode('utf-8'))
         serializer = self.view_config.serializer_class(data=data)
@@ -99,6 +115,8 @@ class CreateView(_ABCGenericView, CreateViewInterface):
 
 
 class ListView(_ABCGenericView, ListViewInterface):
+    """Generic view that lists all objects in the database"""
+
     def get(self, request):
         model_list = self.view_config.model.objects.all()
         serializer = self.view_config.serializer_class(model_list, many=True)
@@ -107,6 +125,8 @@ class ListView(_ABCGenericView, ListViewInterface):
 
 
 class DetailView(_ABCGenericView, DetailViewInterface):
+    """Generic view that gets a single object in the database"""
+
     def get(self, request, query):
         model_detail = self.view_config.model.objects.get(**query)
         serializer = self.view_config.serializer_class(model_detail)
@@ -115,6 +135,8 @@ class DetailView(_ABCGenericView, DetailViewInterface):
 
 
 class DeleteAllView(_ABCGenericView, DeleteAllViewInterface):
+    """Generic view that deletes all objects in the database"""
+
     def delete(self, request):
         self.view_config.model.objects.all().delete()
 
