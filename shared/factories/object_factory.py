06f419e705665d45af7c6e87c497ce8d34525cb6
Lemuel Guevara <lemueljohn.guevara@gmail.com>
2023-06-12 23:57:40 +0800

    PPS-58 Created an object factory

---
+++ shared/factories/object_factory.py
@@ -0,0 +1,17 @@
+"""This file is responsible for handling the object factories"""
+
+from abc import abstractmethod, ABC
+
+from django.db import models
+
+
+class ObjectFactory(ABC):
+    """Abstract factory that represents creation and updating of an object"""
+
+    @abstractmethod
+    def create_object(self, validated_data) -> models.Model:
+        """Returns a model"""
+
+    @abstractmethod
+    def update_object(self, instance, validated_data):
+        """Updates the object"""
