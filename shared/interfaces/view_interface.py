780c71512a1297aa4bb4d45b9a0875f5a0e5f7fb
Lemuel Guevara <lemueljohn.guevara@gmail.com>
2023-06-19 06:39:30 +0800

    Extracted delete method into a whole new class and replaced JsonResponse with Response

--- shared/interfaces/view_interface.py
+++ shared/interfaces/view_interface.py
@@ -1,4 +1,4 @@
-from django.http import JsonResponse
+from rest_framework.response import Response
 from rest_framework.request import Request
 from abc import ABC, abstractmethod
 
@@ -6,27 +6,20 @@ from abc import ABC, abstractmethod
 class UpdateViewInterface(ABC):
     """Absract interface that defines the methods of an UpdateVIew"""
 
-    def update(self, request: Request, pk=None, look_query=None, partial=False) -> JsonResponse:  # noqa: E501
-        """Gets the object to be updated"""
-
     @abstractmethod
-    def put(self, request: Request, pk=None, look_query=None) -> JsonResponse:
+    def put(self, request: Request, pk=None, lookup_query=None) -> Response:
         """Updates the object fully"""
 
     @abstractmethod
-    def patch(self, request: Request, pk=None, look_query=None) -> JsonResponse:
+    def patch(self, request: Request, pk=None, look_query=None) -> Response:
         """Updates an object partially"""
-
-    @abstractmethod
-    def delete(self, request: Request, pk=None, look_query=None) -> JsonResponse:
-        """Deletes a single object"""
 
 
 class CreateViewInterface(ABC):
     """Abstract interface that defines the methods of a CreateView"""
 
     @abstractmethod
-    def post(self, request: Request) -> JsonResponse:
+    def post(self, request: Request) -> Response:
         """Creates an object"""
 
 
@@ -34,7 +27,7 @@ class ListViewInterface(ABC):
     """Abstract interface that defines the methods of a ListView"""
 
     @abstractmethod
-    def get(self, request: Request) -> JsonResponse:
+    def get(self, request: Request) -> Response:
         """Lists all the objects"""
 
 
@@ -42,13 +35,13 @@ class DetailViewInterface(ABC):
     """Abstract interface that defines the methods of a DetailView"""
 
     @abstractmethod
-    def get(self, request: Request, query: dict) -> JsonResponse:
+    def get(self, request: Request, query: dict) -> Response:
         """Gets a single object"""
 
 
-class DeleteAllViewInterface(ABC):
-    """Abstract interface that defines the methods of a DeleteAllView"""
+class DeleteViewInterface(ABC):
+    """Abstract interface that defines the methods of a DeleteView"""
 
     @abstractmethod
-    def delete(self, request: Request) -> JsonResponse:
-        """Deletes all objects"""
+    def delete(self, request: Request) -> Response:
+        """Either deletes a single object or all objects"""
