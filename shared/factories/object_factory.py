8f6c02e797ede2ff227bc7db0382235ef1c63b43
Lemuel Guevara <lemueljohn.guevara@gmail.com>
2023-06-18 08:00:48 +0800

    Added type generics

--- shared/factories/object_factory.py
+++ shared/factories/object_factory.py
@@ -1,18 +1,19 @@
 """This file is responsible for handling the object factories"""
 
 from abc import abstractmethod, ABC
-from typing import Any
+from typing import TypeVar, Generic
 
 from django.db import models
 
+ModelType = TypeVar('ModelType', bound=models.Model)
 
-class ObjectFactory(ABC):
+class ObjectFactory(ABC, Generic[ModelType]):
     """Abstract factory that represents creation and updating of an object"""
 
     @abstractmethod
-    def create_object(self, validated_data) -> models.Model:
-        """Returns a model"""
+    def create_object(self, validated_data) -> ModelType:
+        """Returns a type of a model"""
 
     @abstractmethod
-    def update_object(self, instance, validated_data) -> Any:
+    def update_object(self, instance, validated_data) -> ModelType:
         """Updates the object"""
