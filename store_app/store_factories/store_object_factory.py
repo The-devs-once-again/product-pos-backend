e9ef35aa1cc19312f8bda86d5e3428b70ff168c0
Lemuel Guevara <lemueljohn.guevara@gmail.com>
2023-06-19 17:31:13 +0800

    Store Object Factory Changes:
    *Revisions and changes*:
    - Revised some of the documentation
    
    *Additions*:
    - Added the ability to append and remove items in the item list of an order
    - Added a method that will dynamically update the billing amount when an order is updated
    - Billing factory

--- store_app/store_factories/store_object_factory.py
+++ store_app/store_factories/store_object_factory.py
@@ -1,21 +1,26 @@
 """This file is responsible for making store object factories"""
 
-from typing import Any
 from django.db import models
-
 from shared.factories.object_factory import ObjectFactory
 from shared.product_app.factories.product_object_factory import ProductFactory
-from store_app.models import Order, Item
-from django.utils.crypto import get_random_string
+from store_app.models import Order, Item, Billing
 
 
-class ItemFactory(ObjectFactory):
-    """Concrete store object factory creates an item object. This factory implements the 
-        abstract ObjectFactory"""
+class ItemFactory(ObjectFactory[Item]):
+    """
+    
+    Concrete store object factory creates an item object. This factory implements the
+    abstract ObjectFactory.
+    
+    """
 
     @staticmethod
     def _check_variation_size(size_list: list) -> bool:
-        """Checks if the size list is empty or not"""
+        """
+        
+        Checks if the size list is empty or not.
+        
+        """
 
         size_exists: bool = False
 
@@ -28,8 +33,11 @@ class ItemFactory(ObjectFactory):
 
     @staticmethod
     def _get_variation_total_amount(size_list, add_on_list) -> tuple[int, int]:
-        """Gets the total amount of the size and add on list. It first checks if it
-        is a dictionary or an instance
+        """
+        
+        Gets the total amount of the size and add on list. It first checks if it
+        is a dictionary or an instance.
+        
         """
 
         size_price_total = 0
@@ -37,28 +45,34 @@ class ItemFactory(ObjectFactory):
 
         if isinstance(size_list, models.Manager):
             size_list = size_list.all()
+        
         for size_price in size_list:
             if isinstance(size_price, dict):
-                size_price_total += size_price['size_price']
+                size_price_total += size_price["size_price"]
             else:
                 size_price_total += size_price.size_price
 
         if isinstance(add_on_list, models.Manager):
             add_on_list = add_on_list.all()
+        
         for add_on_price in add_on_list:
             if isinstance(add_on_price, dict):
-                add_on_price_total += add_on_price['add_on_price']
+                add_on_price_total += add_on_price["add_on_price"]
             else:
                 add_on_price_total += add_on_price.add_on_price
 
         return size_price_total, add_on_price_total
 
     def _get_item_total(self, product_data, item_quantity: int) -> float:
-        """"Gets the item total by adding the vartion field prices"""
+        """"
+        
+        Gets the item total by adding the vartion field prices.
+        
+        """
 
         if isinstance(product_data, dict):
-            product_price = product_data['product_price']
-            product_variation = product_data.get('variation', None)
+            product_price = product_data["product_price"]
+            product_variation = product_data.get("variation", None)
         else:
             product_price = product_data.product_price
             product_variation = product_data.variation
@@ -68,15 +82,17 @@ class ItemFactory(ObjectFactory):
                 variation_copy = dict(product_variation)
             else:
                 variation_copy = {
-                    'size': product_variation.size,
-                    'add_on': product_variation.add_on
+                    "size": product_variation.size,
+                    "add_on": product_variation.add_on,
                 }
 
-            size_list = variation_copy.get('size', [])
-            add_on_list = variation_copy.get('add_on', [])
+            size_list = variation_copy.get("size", [])
+            add_on_list = variation_copy.get("add_on", [])
 
             variation_size = self._check_variation_size(size_list)
-            size_price_total, add_price_total = self._get_variation_total_amount(size_list, add_on_list)
+            size_price_total, add_price_total = self._get_variation_total_amount(
+                size_list, add_on_list
+            )
 
             if variation_size and add_on_list:
                 item_total = size_price_total + add_price_total
@@ -89,26 +105,39 @@ class ItemFactory(ObjectFactory):
         else:
             return product_price * item_quantity
 
-    def create_object(self, validated_data) -> models.Model:
-        """"Creates the item object the returns an item instance"""
+    def create_object(self, validated_data) -> Item:
+        """"
+        
+        Creates the item object the returns an item instance.
+        
+        """
 
-        product_data = validated_data.pop('product')
-        item_quantity = validated_data['item_quantity']
+        product_data = validated_data.pop("product")
+        item_quantity = validated_data["item_quantity"]
         item_amount = self._get_item_total(product_data, item_quantity)
 
         product = ProductFactory().create_object(product_data)
 
-        item = Item.objects.create(amount=item_amount, item_quantity=item_quantity, product=product)
+        item = Item.objects.create(
+            amount=item_amount, item_quantity=item_quantity, product=product
+        )
 
         return item
 
-    def update_object(self, instance, validated_data) -> Any:
+    def update_object(self, instance, validated_data) -> Item:
+        """
+        
+        Updates the existing item instance then returns a updated
+        item instance.
+        
+        """
+
         # Update the item fields
-        item_quantity = validated_data['item_quantity']
+        item_quantity = validated_data["item_quantity"]
         instance.item_quantity = item_quantity
 
         # Update product
-        product_data = validated_data.get('product')
+        product_data = validated_data.get("product")
         if product_data:
             product = instance.product
             ProductFactory().update_object(product, product_data)
@@ -120,42 +149,143 @@ class ItemFactory(ObjectFactory):
         return instance
 
 
-class OrderFactory(ObjectFactory):
-    """Concrete store object factory creates an order object. This factory implements 
-        the abstract ObjectFactory"""
+class OrderFactory(ObjectFactory[Order]):
+    """
+    
+    Concrete store object factory creates an order object. 
+    This factory implements the abstract ObjectFactory.
+    
+    """
 
-    def create_object(self, validated_data) -> models.Model:
-        item_list_data = validated_data.pop('item_list', [])
+    def create_object(self, validated_data) -> Order:
+        """
+        
+        Creates the order object then returns a order instance.
+        
+        """
+
+        item_list_data = validated_data.pop("item_list", [])
 
         item_list = []
         for item_data in item_list_data:
             item = ItemFactory().create_object(item_data)
             item_list.append(item)
 
-        order_id = get_random_string(length=6).upper()
         order_quantity = len(item_list)
 
-        order = Order.objects.create(order_id=order_id, order_quantity=order_quantity, order_status="ONGOING")
+        order = Order.objects.create(
+            order_quantity=order_quantity, order_status="ONGOING"
+        )
         order.item_list.set(item_list)
 
         return order
 
-    def update_object(self, instance, validated_data) -> Any:
+    def update_object(self, instance, validated_data) -> Order:
+        """
+        
+        Updates the order object then returns a update order instance.
+        
+        """
+
         # Update order fields
-        instance.order_date = validated_data.get('order_date', instance.order_date)
-        instance.order_id = validated_data.get('order_id', instance.order_id)
-        instance.order_quantity = validated_data.get('order_quantity', instance.order_quantity)
-        instance.order_status = validated_data.get('order_status', instance.order_status)
+        instance.order_quantity = validated_data.get(
+            "order_quantity", instance.order_quantity
+        )
+        instance.order_status = validated_data.get(
+            "order_status", instance.order_status
+        )
 
         # Update items
-        item_list = validated_data.get('item_list')
-        if item_list:
-            for item_data in item_list:
-                product_code = item_data['product']['product_code']
-                item = Item.objects.filter(product__product_code=product_code).first()
+        item_list = validated_data.get("item_list", instance.item_list)
+        for item_data in item_list:
+            item_uuid = item_data.get("uuid")
+
+            if item_uuid is not None:
+                item = Item.objects.filter(uuid=item_uuid).first()
+
                 if item:
                     ItemFactory().update_object(item, item_data)
-
+            else:
+                item = ItemFactory().create_object(item_data)
+                instance.item_list.add(item)
+        
         instance.save()
+
+        # Remove items 
+        remove_item_list = validated_data.get("remove_item_list", [])
+        for item_uuid in remove_item_list:
+            item = Item.objects.filter(uuid=item_uuid).first()
+            if item:
+                instance.item_list.remove(item)
+
+        return instance
+    
+    def update_billing_amount(self, order: Order):
+        """
+        
+        Updates the billing total amount
+        
+        """
+
+        billing = Billing.objects.filter(order=order).first()
+
+        if billing:
+            billing.amount = sum(item.amount for item in order.item_list.all())
+            billing.save()
+
+
+class BillingFactory(ObjectFactory[Billing]):
+    """
+    
+    Create store object that creates a billing object. This factory implements
+    the abstract ObjectFactory.
+    
+    """
+
+    @staticmethod
+    def _get_billing_total(order: Order) -> float:
+        """
+        
+        Gets the total billing amount upon creating the object. NOTE that this
+        does not dynamically update when updating orders so the 
+        'update_billing_amount' above is responsible when the item list of the
+        order is changing.
+        
+        """
+
+        item_list = order.item_list.all()
+        order_total = 0
+
+        for item in item_list:
+            order_total += item.amount
+
+        return order_total
+
+    def create_object(self, validated_data) -> Billing:
+        """
+        
+        Creates the billing object
+        
+        """
+
+        order_data = validated_data.get("order")
+        order = OrderFactory().create_object(order_data)
+
+        amount = self._get_billing_total(order)
+        payment_method = "CASH"
+ 
+        billing = Billing.objects.create(
+            payment_method=payment_method, amount=amount, order=order
+        )
+
+        return billing
+
+    def update_object(self, instance, validated_data) -> Billing:
+        """
+        
+        Update the billing object. A of the moment this is empty but
+        can be used in the future for scalability purposes.
+        
+        """
 
         return instance
