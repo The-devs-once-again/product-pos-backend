52966a880ca80eb134bab98d5ac69e453a05bdd1
Lemuel Guevara <lemueljohn.guevara@gmail.com>
2023-06-19 18:12:14 +0800

    View Factory Changes:
    Revisions:
    - Updated the documentation with more information
    
    Additions:
    - None
    
    NOTE:
    This factory will now be converted to a legacy feature as the current standard of making views in this project is the ModelViewSet

--- shared/factories/view_factory.py
+++ shared/factories/view_factory.py
@@ -2,36 +2,46 @@
 
 from abc import ABC, abstractmethod
 from dataclasses import dataclass
-from typing import Type
 
 from rest_framework.views import APIView
 
-from .generic_views import UpdateView, CreateView, ListView, DetailView, DeleteAllView
+from ..base_views.generic_views import DeleteView, UpdateView, CreateView, ListView, DetailView
 from shared.configs.view_config import GenericViewConfigLoader, ViewConfig
 
 
 class ViewFactory(ABC):
-    """Abstract factory that represents generic views.
+    """
+    
+    Abstract factory that represents generic views.
 
     This class defines a common interface for creating instances of different views.
     Subclasses of this class must implement the `get_view` method to create and return
     instances of specific views.
+    
     """
 
     @abstractmethod
     def get_view(self, view_name: str):
-        """Returns a new view instance"""
+        """
+        
+        Returns a new view instance.
+        
+        """
 
 
 class BaseView(GenericViewConfigLoader, ViewFactory):
-    """Concrete factory for creating instances of the generic views"""
+    """
+    
+    Concrete factory for creating instances of the generic views.
+    
+    """
 
     views = {
         'update_view': UpdateView,
         'create_view': CreateView,
         'list_view': ListView,
         'detail_view': DetailView,
-        'delete_all_view': DeleteAllView,
+        'delete_view': DeleteView
     }
 
     def get_view(self, view_name: str):
@@ -42,13 +52,16 @@ class BaseView(GenericViewConfigLoader, ViewFactory):
 
 @dataclass
 class GenericViewFactory(APIView):
-    """Concrete factory for returning the generic view instances form the
-        BaseView factory.
-        
-        This class takes in a ViewConfig then initializes the BaseView.
-        The initialized views can be access through the use of properties
-        which return their responding view types.
-        """
+    """
+    
+    Concrete factory for returning the generic view instances form the
+    BaseView factory.
+    
+    This class takes in a ViewConfig then initializes the BaseView.
+    The initialized views can be access through the use of properties
+    which return their responding view types.
+
+    """
 
     view_config: ViewConfig
 
@@ -56,21 +69,57 @@ class GenericViewFactory(APIView):
         self.__base_view = BaseView(view_config=self.view_config)
 
     @property
-    def update_view(self) -> Type[UpdateView]:
+    def update_view(self) -> UpdateView:
+        """
+        
+        A property that returns an instance of the UpdateView
+        with its that inherits from the base view config.
+
+        """
+
         return self.__base_view.get_view('update_view')
 
     @property
-    def create_view(self) -> Type[CreateView]:
+    def create_view(self) -> CreateView:
+        """
+        
+        A property that returns an instance of the CreateView
+        with its that inherits from the base view config.
+
+        """
+
         return self.__base_view.get_view('create_view')
 
     @property
-    def list_view(self) -> Type[ListView]:
+    def list_view(self) -> ListView:
+        """
+        
+        A property that returns an instance of the ListView
+        with its that inherits from the base view config.
+
+        """
+
         return self.__base_view.get_view('list_view')
 
     @property
-    def detail_view(self) -> Type[DetailView]:
+    def detail_view(self) -> DetailView:
+        """
+        
+        A property that returns an instance of the UpdateView
+        with its that inherits from the base view config.
+
+        """
+
+
         return self.__base_view.get_view('detail_view')
 
     @property
-    def delete_all_view(self) -> Type[DeleteAllView]:
-        return self.__base_view.get_view('delete_all_view')
+    def delete_view(self) -> DeleteView:
+        """
+        
+        A property that returns an instance of the DeleteView 
+        with its that inherits from the base view config.
+
+        """
+
+        return self.__base_view.get_view('delete_view')
